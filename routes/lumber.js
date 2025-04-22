6f1ef1ea5f42cd53e8d2cb74c30eaee8de9ada1a
Cody Garner <99354149+WinterGelom@users.noreply.github.com>
2025-04-22 18:20:55 -0500

    Added secure to the update page

--- routes/lumber.js
+++ routes/lumber.js
@@ -2,6 +2,12 @@ var express = require('express');
 const lumber_controlers=require("../controllers/lumber_collection")
 var router = express.Router();
 
+const secured = (req, res, next) => {
+  if(req.user){
+    return next();
+  }
+  res.redirect("/login");
+}
 /* GET home page. */
 /*
 router.get('/', function(req, res, next) {
@@ -12,6 +18,6 @@ router.get('/', function(req, res, next) {
 router.get('/', lumber_controlers.lumber_view_all_Page);
 router.get('/detail', lumber_controlers.lumber_view_one_Page);
 router.get('/create', lumber_controlers.lumber_create_Page);
-router.get('/update', lumber_controlers.lumber_update_Page);
+router.get('/update', secured, lumber_controlers.lumber_update_Page);
 router.get('/delete', lumber_controlers.lumber_delete_Page);
 module.exports = router;
