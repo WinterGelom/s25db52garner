d66672a29fb3cd17ce3d7c2275275c899d05f6bf
Cody Garner <99354149+WinterGelom@users.noreply.github.com>
2025-04-23 19:05:20 -0500

    Created the buttons to get around the page on the lumber page

--- models/lumber.js
+++ models/lumber.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose")
 const lumberSchema = mongoose.Schema({
     lumber_type: String,
-    length: Number,
+    length: {
+        type:Number,
+        min: [4, 'Too short of a lumber'],
+        max: [50, 'Too long of a lumber']
+    },
     cost: Number
 })
 
--- routes/lumber.js
+++ routes/lumber.js
@@ -17,7 +17,7 @@ router.get('/', function(req, res, next) {
 // GET Lumbers
 router.get('/', lumber_controlers.lumber_view_all_Page);
 router.get('/detail', lumber_controlers.lumber_view_one_Page);
-router.get('/create', lumber_controlers.lumber_create_Page);
+router.get('/create', secured, lumber_controlers.lumber_create_Page);
 router.get('/update', secured, lumber_controlers.lumber_update_Page);
-router.get('/delete', lumber_controlers.lumber_delete_Page);
+router.get('/delete', secured, lumber_controlers.lumber_delete_Page);
 module.exports = router;
